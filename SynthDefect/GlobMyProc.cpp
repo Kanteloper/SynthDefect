0fa9e1203df2f768df72c33045af4a45306cdfc7
nam4943 <nam4943@ajou.ac.kr>
2020-07-29 13:22:52 +0900

    DEL: MyFileOpen, getFileFormat method ADD: ConvertStdString method

--- SynthDefect/GlobMyProc.cpp
+++ SynthDefect/GlobMyProc.cpp
@@ -3,76 +3,32 @@
 #include "pch.h"
 
 /// <summary>
-/// Open a file of target path
-/// </summary>
-/// <param name="fPath">: the path of a file</param>
-/// <param name="mode">: type of access permitted</param>
-/// <returns> the pointer of FILE if success; NULL on failure </returns>
-FILE* MyFileOpen(_TCHAR* fPath, _TCHAR* mode)
-{
-	FILE* fp = NULL;
-	errno_t err = _tfopen_s(&fp, fPath, mode);
-	if (!err)
-		return fp;
-	else
-		return NULL;
-}
-
-
-/// <summary>
-/// extract file format from file path
-/// </summary>
-/// <param name="path">: Relative or Absolute path of selected file </param>
-/// <returns> file </returns>
-CString getFileFormat(LPCTSTR path)
-{
-	int curPos = 0;
-
-	// In order to for the macros to have space to store the a temporary length, 
-	// it is necessary to declare a local variable called '_convert'
-	// See https://docs.microsoft.com/ko-kr/cpp/mfc/tn059-using-mfc-mbcs-unicode-conversion-macros?view=vs-2019
-	// convert lpctstr(MBCS) to lpcstr(MBCS), convert lpcstr(MBCS) to CString
-	USES_CONVERSION;
-	CString tmpStr = T2CW(path);
-	curPos = tmpStr.Find(_T("."));
-	tmpStr = tmpStr.Tokenize(_T("."), curPos);
-	return tmpStr;
-}
-
-
-/// <summary>
 /// Load selected object file
 /// </summary>
 /// <param name="pathName">: the path of selected file </param>
 /// <returns> TRUE if load success, FALSE on failure </returns>
 BOOL LoadModel(LPCTSTR pathName)
 {
-	// Check file format
-	CString fileFormat = getFileFormat(pathName);
+	Assimp::Importer import;
+	const aiScene* scene = import.ReadFile(ConvertStdString(pathName), aiProcess_Triangulate | aiProcess_FlipUVs);
 
-	// Possible to add other fileformat of Mesh
-	if (fileFormat == "obj")
-	{
-		FILE* file;
-		file = MyFileOpen((LPWSTR)pathName, _T("r"));
-
-		while (1)
-		{
-			TCHAR lineHeader[128];
-			int res;
-			// Read the first word of the line
-			if ((res = _ftscanf_s(file, _T("%s"), lineHeader, static_cast<unsigned int>_countof(lineHeader))) == EOF)
-				break;
-
-			// Parse line header
-
-		}
-	}
-	else
-	{
-		AfxMessageBox(L"Not support \'" + fileFormat , MB_OK | MB_ICONERROR);
-		return FALSE;
-	}
 
 	return TRUE;
+}
+
+
+/// <summary>
+/// Convert LPCTSTR to std::string
+/// </summary>
+/// <param name="target">: Target LPCTSTR of conversion </param>
+/// <returns> Converted value of std::string type </returns>
+std::string ConvertStdString(LPCTSTR target)
+{
+	// In order to for the macros to have space to store the a temporary length, 
+	// See https://docs.microsoft.com/ko-kr/cpp/mfc/tn059-using-mfc-mbcs-unicode-conversion-macros?view=vs-2019
+	// convert lpctstr(MBCS) to lpcstr(MBCS), convert lpcstr(MBCS) to CString
+	USES_CONVERSION;
+	CString tmpStr = T2CW(target);
+	std::string result((CStringA)tmpStr);
+	return result;
 }
--- SynthDefect/GlobMyProc.h
+++ SynthDefect/GlobMyProc.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
+
 CString getFileFormat(const char* path);
 FILE* MyFileOpen(_TCHAR* fPath, _TCHAR* mode);
 BOOL LoadModel(LPCTSTR pathName);
+std::string ConvertStdString(LPCTSTR target);
