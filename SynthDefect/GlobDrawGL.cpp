9e9eb7d257680b9ecbfbe7d0afb9b70a551a9256
nam4943 <nam4943@ajou.ac.kr>
2020-07-30 15:50:54 +0900

    MOD: InitGL, DrawGLScene method

--- SynthDefect/GlobDrawGL.cpp
+++ SynthDefect/GlobDrawGL.cpp
@@ -2,27 +2,23 @@
 
 #include "pch.h"
 
-// All Setup for OpenGL
+// All global Setup for OpenGL
 void InitGL()
 {
 	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
-	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
-	glClearDepth(1.0f);									// Depth Buffer Setup
 	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
 
 	// The Type Of Depth Testing To Do
 	// GL_LEQUAL : Passes if the incoming depth value is less than or equal to the stored depth value.
 	glDepthFunc(GL_LEQUAL);
-
-	// Specify implementation-specific hints
-	// Certain aspects of GL behavior can be controlled with hints.
-	// GL_PERSPECTIVE_CORRECTION_HINT: Indicates the quality of color, texture coordinate, and fog(¾È°³) coordinate interpolation.
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
 int DrawGLScene()
 {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearDepth(1.0f);									// Depth Buffer Setup
+
 	glLoadIdentity();									// Reset the current ModelView Matrix
 	glTranslatef(-1.5f, 0.0f, -6.0f);						// Move Left 1.5 Units And Into The Screen 6.0
 	glBegin(GL_TRIANGLES);								// Drawing Using Triangles
