2add16dbddd67be545b2181fac381784fdbc775b
nam4943 <nam4943@ajou.ac.kr>
2020-07-31 15:31:29 +0900

    MOD: making Shader class

--- SynthDefect/Shader.cpp
+++ SynthDefect/Shader.cpp
@@ -1,4 +1,18 @@
 #include "pch.h"
 #include "Shader.h"
 
+CShader::CShader(const char* vertexPath, const char* fragmentPath)
+{
+	// retrieve the vertex/fragment shader code
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
 
+	// 
+
+}
+
+CShader::~CShader()
+{
+}
--- SynthDefect/Shader.h
+++ SynthDefect/Shader.h
@@ -3,8 +3,24 @@
 
 #pragma once
 
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
 class CShader
 {
+	// Constructor
+public:
+	CShader(const char* vertexPath, const char* fragmentPath);
+
+	// Attributes
+public:
+	unsigned int ID;			// the program ID
+
+	// Implement
+public:
+	~CShader();
 
 };
 
