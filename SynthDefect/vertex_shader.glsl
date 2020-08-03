58ccaafe557abd5b509cb55c4fc5964c2b3a4a3f
nam4943 <nam4943@ajou.ac.kr>
2020-08-03 18:09:48 +0900

    MOD: sample code for vertex shader

--- SynthDefect/vertex_shader.glsl
+++ SynthDefect/vertex_shader.glsl
@@ -7,9 +7,9 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
-
 // out - the storage qualifier which linkage out of a shader to a subsequent stage, variable is copied out
 out vec2 TexCoords;
+out vec4 vertexColor;
 
 // uniform - the storage qualifier which means the value does not change accross the primitive being processed
 uniform mat4 model;
@@ -19,5 +19,7 @@ uniform mat4 projection;
 void main()
 {
     TexCoords = aTexCoords;    
+    // gl_Position is built-in variable in vertex language
     gl_Position = projection * view * model * vec4(aPos, 1.0);
+    vertexColor = vec4(0.5, 0.5, 0.5, 1.0);
 }
