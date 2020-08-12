3b7046609e8c90c5cdddda2162513ea9dafdb6d5
nam4943 <nam4943@ajou.ac.kr>
2020-08-12 16:46:21 +0900

    MOD: add ambient lightning and diffuse lightning

--- SynthDefect/model_fs.glsl
+++ SynthDefect/model_fs.glsl
@@ -1,6 +1,8 @@
 #version 460
 
-in vec4 vertexColor; 
+in vec4 vertexColor;
+in vec3 Normal;
+in vec3 FragPos;
 out vec4 FragColor;
 
 uniform vec3 lightColor;
@@ -8,8 +10,14 @@ uniform vec3 lightPos;
 
 void main()
 {    
-	float ambientStrength = 1.0;
+	// ambient lightning
+	float ambientStrength = 0.4;
 	vec3 ambient = ambientStrength * lightColor;
-	vec4 result = vec4(ambient, 1.0) * vertexColor;
-	FragColor = result;
+	// diffuse lightning
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+	float diff = max(dot(norm, lightDir), 0.0);		// note that max function is for always positive value
+	vec3 diffuse = diff * lightColor;
+	vec3 result = (ambient + diffuse) * vec3(vertexColor);
+	FragColor = vec4(result, 1.0);
 }
