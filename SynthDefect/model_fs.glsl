d37633a0ae271c5425857bc504215db4b883ea77
nam4943 <nam4943@ajou.ac.kr>
2020-08-12 15:57:35 +0900

    ADD: make ambient lightning

--- SynthDefect/model_fs.glsl
+++ SynthDefect/model_fs.glsl
@@ -1,1 +1,15 @@
-d
+#version 460
+
+in vec4 vertexColor; 
+out vec4 FragColor;
+
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+
+void main()
+{    
+	float ambientStrength = 1.0;
+	vec3 ambient = ambientStrength * lightColor;
+	vec4 result = vec4(ambient, 1.0) * vertexColor;
+	FragColor = result;
+}
