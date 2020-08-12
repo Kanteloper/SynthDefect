491b2e5ce2ff7012fb025f2fedbe5bf320b903d2
nam4943 <nam4943@ajou.ac.kr>
2020-08-12 15:57:11 +0900

    MOD: process only color in fragment shader

--- SynthDefect/background_fs.glsl
+++ SynthDefect/background_fs.glsl
@@ -3,13 +3,7 @@
 in vec4 vertexColor; 
 out vec4 FragColor;
 
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-
 void main()
 {    
-	float ambientStrength = 1.0;
-	vec3 ambient = ambientStrength * lightColor;
-	vec4 result = vec4(ambient, 1.0) * vertexColor;
-	FragColor = result;
+	FragColor = vertexColor;
 }
