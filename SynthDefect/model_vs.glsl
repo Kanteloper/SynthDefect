cba8fe58b2e4fd5b3683a164ec29eb6abe6f31d5
nam4943 <nam4943@ajou.ac.kr>
2020-08-12 16:45:49 +0900

    ADD: 1. input and ouput vertex normal in vertex shader 2. calculate fragment position for lightning

--- SynthDefect/model_vs.glsl
+++ SynthDefect/model_vs.glsl
@@ -6,8 +6,11 @@
 //          for configuring the vertex attributes on the CPU
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aColor;
+layout (location = 2) in vec3 aNormal;
 // out - the storage qualifier which linkage out of a shader to a subsequent stage, variable is copied out
 out vec4 vertexColor;
+out vec3 Normal;
+out vec3 FragPos;
 
 // uniform - the storage qualifier which means the value does not change accross the primitive being processed
 uniform mat4 model;
@@ -18,5 +21,7 @@ void main()
 {
     // gl_Position is built-in variable in vertex language
     gl_Position = projection * view * model * vec4(aPos, 1.0);
+    FragPos = vec3(model * vec4(aPos, 1.0));
     vertexColor = aColor;
+    Normal = aNormal;
 }
