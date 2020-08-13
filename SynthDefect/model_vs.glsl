03240d9e8258c30af128836e3a818fef3c5c425d
kanteloper <com4943@gmail.com>
2020-08-13 23:04:54 +0900

    MOD: transform the normal vectors to world space coordinates

--- SynthDefect/model_vs.glsl
+++ SynthDefect/model_vs.glsl
@@ -23,5 +23,5 @@ void main()
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     FragPos = vec3(model * vec4(aPos, 1.0));
     vertexColor = aColor;
-    Normal = aNormal;
+    Normal = mat3(transpose(inverse(model))) * aNormal;
 }
