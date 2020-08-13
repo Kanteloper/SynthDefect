8059b5b4c632992941145b288edebf561ab2612e
kanteloper <com4943@gmail.com>
2020-08-13 22:51:06 +0900

    ADD: specular lightning

--- SynthDefect/model_fs.glsl
+++ SynthDefect/model_fs.glsl
@@ -7,17 +7,27 @@ out vec4 FragColor;
 
 uniform vec3 lightColor;
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
 {    
 	// ambient lightning
 	float ambientStrength = 0.4;
 	vec3 ambient = ambientStrength * lightColor;
+
 	// diffuse lightning
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);		// note that max function is for always positive value
 	vec3 diffuse = diff * lightColor;
-	vec3 result = (ambient + diffuse) * vec3(vertexColor);
+
+	// specular lightning
+	float specularStrength = 1.0;
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 2);
+	vec3 specular = specularStrength * spec * lightColor;
+
+	vec3 result = (ambient + diffuse + specular) * vec3(vertexColor);
 	FragColor = vec4(result, 1.0);
 }
