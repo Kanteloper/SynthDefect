1f9c919104422468acaa676aa5f2555936ec853d
nam4943 <nam4943@ajou.ac.kr>
2020-08-14 19:17:20 +0900

    MOD: light position

--- SynthDefect/GLRoutine.h
+++ SynthDefect/GLRoutine.h
@@ -10,15 +10,14 @@
 
 #define VS_BACKGROUND_PATH	"./background_vs.glsl"		// The current path of vertex shader code file for background mesh
 #define FS_BACKGROUND_PATH	"./background_fs.glsl"		// The current path of fragment shader code file for background mesh
-#define VS_MODEL_PATH		"./model_vs.glsl"			// The current path of fragment shader code file for background mesh
-#define FS_MODEL_PATH		"./model_fs.glsl"			// The current path of fragment shader code file for background mesh
-
+#define VS_MODEL_PATH		"./model_vs.glsl"			// The current path of vertex shader code file for model mesh
+#define FS_MODEL_PATH		"./model_fs.glsl"			// The current path of fragment shader code file for model mesh
 
 /******************* Constants for Light Source *******************/
 
 const float LIGHT_X = 0.0f;
-const float LIGHT_Y = 20.0f;
-const float LIGHT_Z = 0.0f;
+const float LIGHT_Y = 0.0f;
+const float LIGHT_Z = 30.0f;
 
 bool CreateGLWindow(CWnd* pWnd, int bits);
 bool KillGLWindow(CWnd* pWnd);
