072ba8c3e1a09dd0b5cb8786fde5837d90fb9b1a
nam4943 <nam4943@ajou.ac.kr>
2020-08-14 10:52:51 +0900

    ADD: 1. some comments 2. make updateCameraVectors method

--- SynthDefect/Camera.cpp
+++ SynthDefect/Camera.cpp
@@ -8,6 +8,23 @@ CCamera::CCamera()
 {
 }
 
+
+/// <summary>
+/// Calculate the forward vector(direction vector) from the Camera's (updated) Euler Angles
+/// </summary>
+void CCamera::UpdateCameraVectors()
+{
+	// calculate the new Forward vector
+	glm::vec3 forward;
+	forward.x = cos(glm::radians(YAW)) * cos(glm::radians(PITCH));
+	forward.y = sin(glm::radians(PITCH));
+	forward.z = sin(glm::radians(YAW)) * cos(glm::radians(PITCH));
+	m_ForwardAxis = glm::normalize(forward);
+	// re-calculate the Left and Up vector
+	m_LeftAxis = glm::normalize(glm::cross(m_ForwardAxis, m_UpDirection));
+	m_UpAxis = glm::cross(m_LeftAxis, m_ForwardAxis);
+}
+
 /// <summary>
 /// Return the Model-View Matrix calculated using the LookAt Matrix
 /// </summary>
@@ -20,11 +37,20 @@ glm::mat4 CCamera::GetViewMatrix()
 }
 
 
+/// <summary>
+/// Set camera position
+/// </summary>
+/// <param name="pos">: camera position that want to set </param>
 void CCamera::SetPosition(glm::vec3 pos)
 {
 	m_Position = pos;
 }
 
+
+/// <summary>
+/// Retrieve camera position
+/// </summary>
+/// <returns> currently set the camera position vector </returns>
 glm::vec3 CCamera::GetPosition()
 {
 	return m_Position;
@@ -32,7 +58,7 @@ glm::vec3 CCamera::GetPosition()
 
 
 /// <summary>
-/// Return the constant value of Field of View
+/// Retrieve the constant value of Field of View
 /// </summary>
 /// <returns> the value of FOV </returns>
 const float CCamera::GetFOV()
@@ -40,6 +66,11 @@ const float CCamera::GetFOV()
 	return FOV;
 }
 
+
+/// <summary>
+/// Retrieve the constant value of Sensitivity of Camera
+/// </summary>
+/// <returns> the value of SENSITIVITY </returns>
 const float CCamera::GetSensitivity()
 {
 	return SENSITIVITY;
