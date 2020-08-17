f4adad4222149dba2da1cda6826cebd8ed88ee2f
kanteloper <com4943@gmail.com>
2020-08-18 00:30:04 +0900

    MOD: resolve wrong lightning issue

--- SynthDefect/model_fs.glsl
+++ SynthDefect/model_fs.glsl
@@ -3,12 +3,11 @@
 in vec4 vertexColor;
 in vec3 Normal;
 in vec3 FragPos;
-in vec3 lightPos;
 out vec4 FragColor;
 
 uniform vec3 lightColor;
+uniform vec3 lightPos;
 uniform vec3 viewPos;
-//uniform vec3 lightPos;
 
 void main()
 {    
@@ -19,12 +18,12 @@ void main()
 	// diffuse lightning
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
-	float diff = max(dot(norm, lightDir), 0.0);		// note that max function is for always positive value
+	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * lightColor;
 
 	// specular lightning
 	float specularStrength = 0.7;
-	vec3 viewDir = normalize(viewPos - lightPos);
+	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 8);
 	vec3 specular = specularStrength * spec * lightColor;
