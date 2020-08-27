099da582e283517c24be0b3c2484bb904bea32e8
nam4943 <nam4943@ajou.ac.kr>
2020-08-27 15:35:21 +0900

    ADD: constant for ray picking

--- SynthDefect/GlobMyProc.h
+++ SynthDefect/GlobMyProc.h
@@ -2,5 +2,9 @@
 
 #include <string>
 
+/******************* Constants for Ray Picking *******************/
+
+const float MAX_DISTANCE = 100000.0f;
+
 std::string ConvertStdString(LPCTSTR target);
 std::string RetrieveCode(const char* filePath);
