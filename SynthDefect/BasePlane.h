26be9eb8f59318c8659210f00cdc0efd0a394d94
kanteloper <com4943@gmail.com>
2020-08-31 23:34:12 +0900

    ADD: 1. FindMinMaxVertex method for calculating Centroid 2. GetBaseCentroid method for positioning the origin of camera

--- SynthDefect/BasePlane.h
+++ SynthDefect/BasePlane.h
@@ -16,13 +16,17 @@ private:
 	unsigned int m_VAO;
 	unsigned int m_VBO;
 	unsigned int m_EBO;
+	glm::vec3 m_max;
+	glm::vec3 m_min;
 
 	// Implementation
 private:
 	void SetBasePlane();
 	virtual void SetupMesh();
+	void FindMinMaxVertex(glm::vec3 const& p);
 public:
 	~CBasePlane();
 	virtual void Draw();
+	glm::vec3 GetBaseCentroid();
 };
 
