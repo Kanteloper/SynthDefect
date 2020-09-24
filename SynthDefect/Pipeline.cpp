8755703a0e5ad5225fbfbbc4174de730dae227c0
kanteloper <com4943@gmail.com>
2020-09-24 20:16:23 +0900

    ADD: initiate member variables

--- SynthDefect/Pipeline.cpp
+++ SynthDefect/Pipeline.cpp
@@ -1,2 +1,22 @@
 #include "pch.h"
 #include "Pipeline.h"
+
+#include <iostream>
+
+CPipeline::CPipeline()
+{
+	m_base = nullptr;
+	m_model = nullptr;
+	m_numOfDefects = 0;
+}
+
+/// <summary>
+/// 
+/// </summary>
+void CPipeline::Execute()
+{
+}
+
+CPipeline::~CPipeline()
+{
+}
