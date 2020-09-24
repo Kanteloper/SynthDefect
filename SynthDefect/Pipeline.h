2f00ca850e8e259789ba17cae2cec82df0813a8f
kanteloper <com4943@gmail.com>
2020-09-24 20:15:50 +0900

    ADD: 1. constructors and member variables 2. Execute method

--- SynthDefect/Pipeline.h
+++ SynthDefect/Pipeline.h
@@ -1,5 +1,29 @@
+
+// Pipeline.h : Interface of Pipelice class
+
 #pragma once
+
+#include <vector>
+#include "Model.h"
+
 class CPipeline
 {
+	// Constructor
+public:
+	CPipeline();
+	CPipeline(Properties const& props, std::vector<int> const& faces, CModel* model, CModel* base)
+		: m_numOfDefects(props.numOfDefects), m_faces(faces), m_model(model), m_base(base) {}
+	
+	// Attributes
+private:
+	int m_numOfDefects;
+	std::vector<int> m_faces;
+	CModel* m_model;
+	CModel* m_base;
+
+	// implements
+public:
+	void Execute();
+	~CPipeline();
 };
 
