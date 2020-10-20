18470b8c33a705c1cf5cf06ffeee36575f75ef91
kanteloper <com4943@gmail.com>
2020-10-21 00:04:04 +0900

    MOD: complete to make TestDeformation method

--- SynthDefect/SynthDefectView.h
+++ SynthDefect/SynthDefectView.h
@@ -83,7 +83,7 @@ private:
 	BOOL IsPicked(int index);
 	int FindDuplicatedFace(int index);
 	void TestDeformation(CModel* model);
-	float Bernstein(int i, int m, float t, std::array<float, 12> const& nv, int n, int k);
+	float Bernstein(int i, int m, float t, std::array<float, 10> const& nv, int n);
 
 public:
 	virtual ~CSynthDefectView();
