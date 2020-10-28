21ef6ec3936371529576849e43e07b24d5053b7b
kanteloper <com4943@gmail.com>
2020-10-28 22:04:56 +0900

    ADD: implement ExportOBJ method

--- SynthDefect/Base.cpp
+++ SynthDefect/Base.cpp
@@ -1,8 +1,10 @@
 #include "pch.h"
 #include "Base.h"
+#include <assimp/Exporter.hpp>
 
 CBase::CBase(std::string const& filePath)
 {
+	path = filePath;
 	LoadBase(filePath);
 }
 
@@ -25,6 +27,36 @@ void CBase::LoadBase(std::string const& pathName)
 		return;
 	}
 	ProcessNode(scene->mRootNode, scene);
+}
+
+/// <summary>
+/// Export mesh file to OBJ file format
+/// </summary>
+/// <param name="fileName"> The desired file name </param>
+void CBase::ExportOBJ(std::string const& fileName)
+{
+	Assimp::Exporter exporter;
+	Assimp::Importer importer;
+
+	const aiScene* model_scene = importer.ReadFile(path,
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_ValidateDataStructure);
+
+	if (!model_scene || model_scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !model_scene->mRootNode)
+	{
+		TRACE(importer.GetErrorString());
+		return;
+	}
+
+	const aiNode* node = model_scene->mRootNode;
+	for (unsigned int i = 0; i < node->mNumChildren; i++)
+	{
+		SaveNode(node->mChildren[i], model_scene);
+	}
+
+	exporter.Export(model_scene, "obj", "..\\test\\" + fileName + ".obj");
 }
 
 CBase::~CBase()
